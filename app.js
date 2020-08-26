92e34e572c471c35a1f1ad60906762841f00d9b6
Ian James Wilson <wilson.ian26@gmail.com>
2020-08-25 23:57:47 -0500

    changed output path for index.html

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "docs");
+const OUTPUT_DIR = path.resolve(__dirname, "./lib");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
